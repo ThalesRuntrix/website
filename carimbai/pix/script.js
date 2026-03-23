4e87c9447995e63a571633e2ce00a72c4e575dc2
ThalesRuntrix <tech@runtrix.com.br>
2026-03-23 13:32:54 +0000

    -X

--- carimbai/pix/script.js
+++ carimbai/pix/script.js
@@ -1,5 +1,7 @@
 function copyPix() {
     const text = document.getElementById("pixKey").innerText;
     navigator.clipboard.writeText(text);
-    alert("Chave PIX copiada!");
+
+    const btn = document.querySelector(".btn-copy");
+    btn.innerText = "✅ Copiado!";
 }
