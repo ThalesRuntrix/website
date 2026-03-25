73907a4648e19c0839b4012c1361fc5c4ad1a63b
ThalesRuntrix <tech@runtrix.com.br>
2026-03-25 16:51:04 +0000

    update: `/carimbai/produto/style.css`

--- carimbai/produto/script.js
+++ carimbai/produto/script.js
@@ -14,32 +14,35 @@ function renderProduto(produto) {
 
   if (produto.detalhes) {
     for (const key in produto.detalhes) {
-      if (["id", "produto_id", "categoria_id"].includes(key)) continue;      
-      detalhesHTML += `<p><strong>${key.replace("_", " ")}:</strong> ${produto.detalhes[key]}</p>`;
+      if (["id", "produto_id", "categoria_id"].includes(key)) continue;
+
+      detalhesHTML += `
+        <p><strong>${key.replace("_", " ")}:</strong> ${produto.detalhes[key]}</p>
+      `;
     }
   }
 
   container.innerHTML = `
-    <div class="produto-img">
-      <img src="../../img/texto1.jpg" alt="${produto.nome}">
-    </div>
+    <div class="produto-content">
+      
+      <img src="../img/texto.jpg" alt="${produto.nome}">
 
-    <div class="produto-info">
-      <h1>${produto.nome}</h1>
-      <p>Categoria: ${produto.categoria}</p>
+      <div class="produto-title">
+        ${produto.nome}
+      </div>
 
-      <div class="preco">
+      <div class="produto-preco">
         R$ ${Number(produto.preco).toFixed(2)}
       </div>
 
-      <div class="detalhes">
-        <h3>Detalhes</h3>
+      <div class="produto-detalhes">
         ${detalhesHTML}
       </div>
 
-      <a href="#" class="btn-comprar">
-        Comprar via PIX
+      <a class="btn-primary" href="https://wa.me/5511943722620?text=Olá,%20quero%20este%20produto">
+        📲 Comprar via WhatsApp
       </a>
+
     </div>
   `;
 }
