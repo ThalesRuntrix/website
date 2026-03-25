e092ef3acf1773f78d9d8a0d7d1256887a524675
ThalesRuntrix <tech@runtrix.com.br>
2026-03-25 14:23:32 +0000

    bugfix: link to product page

--- carimbai/carimbos/texto/script.js
+++ carimbai/carimbos/texto/script.js
@@ -26,7 +26,7 @@ async function carregarProdutos() {
             R$ ${Number(produto.preco).toFixed(2)}
           </div>
 
-          <a href="../../carimbai/produto.html?id=${produto.id}" class="btn-primary">
+          <a href="../produto.html?id=${produto.id}" class="btn-primary">
           
             Ver detalhes
           </a>
