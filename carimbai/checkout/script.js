00801666b085e48bdcbadbccd9f4992a97ec3308
ThalesRuntrix <tech@runtrix.com.br>
2026-03-31 13:14:50 +0000

    feat:: get product data by id in `checkout page`

--- carimbai/checkout/script.js
+++ carimbai/checkout/script.js
@@ -1,41 +1,55 @@
 function getParam(name) {
-  const params = new URLSearchParams(window.location.search);
-  return params.get(name);
+    const params = new URLSearchParams(window.location.search);
+    return params.get(name);
 }
+
+// 🔥 pegar produto (id vindo do banco de dados)
+const id = getParam("id");
+try {
+    const res = await fetch(`/api/produto/${id}`);
+    const produto = await res.json();
+    console.log("PRODUTO: " + produto);
+
+} catch (error) {
+    console.error(error);    
+}
+
 
 // 🔥 pegar produto (nome vindo da URL)
 const produtoNome = getParam("produto") || "Produto";
-const produtoId = getParam("id") || "id";
+if (produtoNome) {
+    document.getElementById("produto-nome").textContent = produtoNome;
+}
 
 document.getElementById("pedido-form")
-  .addEventListener("submit", function(e) {
-    e.preventDefault();
+    .addEventListener("submit", function (e) {
+        e.preventDefault();
 
-    const form = new FormData(e.target);
+        const form = new FormData(e.target);
 
-    const nome = form.get("nome");
-    const email = form.get("email");
-    const cpf = form.get("cpf");
-    const endereco = form.get("endereco");
-    const entrega = form.get("entrega");
-    const pagamento = form.get("pagamento");
+        const nome = form.get("nome");
+        const email = form.get("email");
+        const cpf = form.get("cpf");
+        const endereco = form.get("endereco");
+        const entrega = form.get("entrega");
+        const pagamento = form.get("pagamento");
 
-    // 🔥 montar mensagem
-    const mensagem = `
-🛒 *NOVO PEDIDO*
+        // 🔥 montar mensagem
+        const mensagem = `
+        🛒 *NOVO PEDIDO*
 
-📦 Produto: ${produtoNome}
+        📦 Produto: ${produtoNome}
 
-👤 Nome: ${nome}
-📧 Email: ${email}
-🪪 CPF: ${cpf}
-📍 Endereço: ${endereco || "Retirada na loja"}
+        👤 Nome: ${nome}
+        📧 Email: ${email}
+        🪪 CPF: ${cpf}
+        📍 Endereço: ${endereco || "Retirada na loja"}
 
-🚚 Entrega: ${entrega}
-💳 Pagamento: ${pagamento}
-    `;
+        🚚 Entrega: ${entrega}
+        💳 Pagamento: ${pagamento}        
+        `;
 
-    const url = `https://wa.me/5511943722620?text=${encodeURIComponent(mensagem)}`;
+        const url = `https://wa.me/5511943722620?text=${encodeURIComponent(mensagem)}`;
 
-    window.location.href = url;
-  });
+        window.location.href = url;
+});
