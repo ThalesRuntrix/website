c92ea60149e122722c373b1b7b7f9b10b8999069
ThalesRuntrix <tech@runtrix.com.br>
2026-04-06 19:55:27 +0000

    bugfix: validate CPF

--- carimbai/checkout/utils/format.js
+++ carimbai/checkout/utils/format.js
@@ -11,37 +11,37 @@ export function getParam(name){
 }
 
 export function validarCPF(cpf){
-  console.warn("VALIDAR CPF - CPF: ", cpf);
-  if (!cpf) return false;  
+  const normalizedCPF = String(cpf).replace(/\D/g, ""); 
+  if (!normalizedCPF) return false;  
 
-  if (cpf.length !== 11) return false;
+  if (normalizedCPF.length !== 11) return false;
 
   // elimina CPFs inválidos conhecidos
-  if (/^(\d)\1+$/.test(cpf)) return false;
+  if (/^(\d)\1+$/.test(normalizedCPF)) return false;
 
   let soma = 0;
   let resto;
 
   // primeiro dígito
   for (let i = 0; i < 9; i++) {
-    soma += parseInt(cpf[i]) * (10 - i);
+    soma += parseInt(normalizedCPF[i]) * (10 - i);
   }
 
   resto = (soma * 10) % 11;
   if (resto === 10) resto = 0;
 
-  if (resto !== parseInt(cpf[9])) return false;
+  if (resto !== parseInt(normalizedCPF[9])) return false;
 
   // segundo dígito
   soma = 0;
   for (let i = 0; i < 10; i++) {
-    soma += parseInt(cpf[i]) * (11 - i);
+    soma += parseInt(normalizedCPF[i]) * (11 - i);
   }
 
   resto = (soma * 10) % 11;
   if (resto === 10) resto = 0;
 
-  if (resto !== parseInt(cpf[10])) return false;
+  if (resto !== parseInt(normalizedCPF[10])) return false;
 
   return true;
       
