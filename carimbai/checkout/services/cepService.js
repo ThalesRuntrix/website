4856c126c1380ff436e20efc821e09d2af95e8dd
ThalesRuntrix <tech@runtrix.com.br>
2026-04-06 16:14:22 +0000

    feat: makes validarCEP() call API

--- carimbai/checkout/services/cepService.js
+++ carimbai/checkout/services/cepService.js
@@ -6,12 +6,11 @@ export const cepService =  {
   // Buscar endereço pelo campo de cep
   async obterEndereco(cep) {
     try {
-      const isCepFormatValid = cepService.validarFormato(cep);      
-      if(isCepFormatValid){
-        const data = await api.buscarCEP(cep); 
-        cepService.setAddressFields(data);
+      const address = await cepService.validarCEP(cep);            
+      if(address){        
+        cepService.setAddressFields(address);
         cepService.blockAddressFieldsEdition(); 
-      }          
+      }       
 
     } catch (err) {
       console.error("Erro no serviço de busca por CEP: ", err);
@@ -39,12 +38,12 @@ export const cepService =  {
 
   async validarCEP(cep) {
     try{
-      const data = await this.obterEndereco(cep);
-
-      if(!data || !data.cidade) return false;
-
-      return data;     
-
+      const isCepFormatValid = cepService.validarFormato(cep);      
+      if(isCepFormatValid){
+        const data = await api.buscarCEP(cep);
+        if(!data || !data.cidade) return false;
+        return data;
+      }
     } catch(error) {
       console.error("Erro ao validar CEP: ", error);
       return false;
