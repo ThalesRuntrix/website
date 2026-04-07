d1efd315f26724f2c17fcfd3c3ea1a3e9eb87f85
ThalesRuntrix <tech@runtrix.com.br>
2026-04-07 14:12:25 +0000

    remove unused import and log

--- carimbai/checkout/services/pedidoService.js
+++ carimbai/checkout/services/pedidoService.js
@@ -3,13 +3,10 @@ import { api } from "../api/api.js";
 export const pedidoService = {
 
     async salvarPedido(dados){
-        console.warn("DADOS: ", dados);
         try {
             return api.postPedido(dados);
         } catch(error){
             console.error("Erro ao salvar pedido:", error);
         }
-    }   
-
-    
+    }      
 }
